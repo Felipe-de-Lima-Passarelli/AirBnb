e96cf29d8312b1a55940f849e0d0559f95df2c73
Felipe <malamdorox@gmail.com>
2025-12-19 13:15:12 -0300

    Configurações de deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/AirBnb",
   assetPrefix: "/AirBnb/",
+  output: "export",
   images: {
     remotePatterns: [
       {
